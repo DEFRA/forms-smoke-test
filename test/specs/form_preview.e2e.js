89faba2ff0ba464815cf9328109c8e6f1f6bbe5b
Jignesh Nayi <jig_nayi@hotmail.com>
2024-08-06 17:15:52 +0100

    Add more checks

--- test/specs/form_preview.e2e.js
+++ test/specs/form_preview.e2e.js
@@ -5,6 +5,20 @@ import FormPage from 'page-objects/FormsRunner.page'
 describe('Forms Preview', () => {
   it('Should preview a form in the runner ', async () => {
     await FormPage.open()
-    await expect(browser).toHaveTitle('Page one - Do not use - Used for Automated Tests - GOV.UK')
+    await expect(browser).toHaveTitle(
+      'Page one - Do not use - Used for Automated Tests - GOV.UK'
+    )
+    const elem = await $('[type="text"]')
+    await elem.setValue('some text')
+    const submitButton = await $('button=Continue')
+    await submitButton.click()
+    await expect(browser).toHaveTitle(
+      'Summary - Do not use - Used for Automated Tests - GOV.UK'
+    )
+    const sendButton = await $('button=Send')
+    await sendButton.click()
+    await expect(browser).toHaveTitle(
+      'Application complete - Do not use - Used for Automated Tests - GOV.UK'
+    )
   })
 })
